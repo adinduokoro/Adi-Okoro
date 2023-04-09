31a19e80d8fef961ee5d5f2eb81099ff9d38f199
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2023-04-09 19:11:21 -0400

    About
    Finished the structuring of the About section. Working on the styling.

---
+++ src/components/about/Info.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div className="about__info grid">
+      <div className="about__box">
+        <i class='bx bx-star about__icon' ></i>
+        <h3 className="about__title">Experience</h3>
+        <span className="about__subtitle">10+ Years Working</span>
+      </div>
+
+      <div className="about__box">
+        <i class='bx bx-star about__icon'></i>
+        <h3 className="about__title">Experience</h3>
+        <span className="about__subtitle">10+ Years Working</span>
+      </div>
+
+      <div className="about__box">
+        <i class='bx bx-star about__icon'></i>
+        <h3 className="about__title">Experience</h3>
+        <span className="about__subtitle">10+ Years Working</span>
+      </div>
+
+    </div>
+  )
+}
+
+export default Info
