1627c7aed0a57a395c78488bf72bd9de104dfad1
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2023-04-09 22:36:10 -0400

    Skills
    About section complete. Working on the skills section.

---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './skills.css'
+
+const Skills = () => {
+  return (
+    <section className="skills section" id="skills">
+      
+    </section>
+  )
+}
+
+export default Skills
