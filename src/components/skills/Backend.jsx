dc8f21a820e07b8a02f8b61c1377d0af7e9f1202
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2023-04-10 09:53:17 -0400

    Skills
    Skills section is almost done. Just need to add in the breakpoints. Service Section will be next.

---
+++ src/components/skills/Backend.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+
+const Backend = () => {
+  return (
+    <div className="skills__content">
+    <h3 className="skills__title">Backend Dev.</h3>
+
+    <div className="skills__box">
+      <div className="skills__group">
+
+        <div className="skills__data">
+          <i class='bx bx-check-circle skills-icon'></i>
+
+          <div>
+            <h3 className="skills__name">Python</h3>
+          </div>
+        </div>
+
+        <div className="skills__data">
+          <i class='bx bx-check-circle skills-icon'></i>
+
+          <div>
+            <h3 className="skills__name">Firebase</h3>
+          </div>
+        </div>
+
+        <div className="skills__data">
+          <i class='bx bx-check-circle skills-icon'></i>
+
+          <div>
+            <h3 className="skills__name">NodeJS</h3>
+          </div>
+        </div>
+
+
+      </div>
+
+      <div className="skills__group">
+        
+        <div className="skills__data">
+          <i class='bx bx-check-circle skills-icon'></i>
+
+          <div>
+            <h3 className="skills__name">MySQL</h3>
+          </div>
+        </div>
+
+        <div className="skills__data">
+          <i class='bx bx-check-circle skills-icon'></i>
+
+          <div>
+            <h3 className="skills__name">C++</h3>
+          </div>
+        </div>
+
+        {/* <div className="skills__data">
+          <i class='bx bx-check-circle skills-icon'></i>
+
+          <div>
+            <h3 className="skills__name"></h3>
+          </div>
+        </div> */}
+
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Backend
---
+++ src/components/skills/Frontend.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+
+const Frontend = () => {
+  return (
+    <div className="skills__content">
+      <h3 className="skills__title">Frontend Dev.</h3>
+
+      <div className="skills__box">
+        <div className="skills__group">
+
+          <div className="skills__data">
+            <i class='bx bx-check-circle skills-icon'></i>
+
+            <div>
+              <h3 className="skills__name">ReactJS</h3>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class='bx bx-check-circle skills-icon'></i>
+
+            <div>
+              <h3 className="skills__name">Javascript</h3>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class='bx bx-check-circle skills-icon'></i>
+
+            <div>
+              <h3 className="skills__name">CSS/SASS</h3>
+            </div>
+          </div>
+
+
+        </div>
+
+        <div className="skills__group">
+          
+          <div className="skills__data">
+            <i class='bx bx-check-circle skills-icon'></i>
+
+            <div>
+              <h3 className="skills__name">HTML</h3>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class='bx bx-check-circle skills-icon'></i>
+
+            <div>
+              <h3 className="skills__name">Bootstrap</h3>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class='bx bx-check-circle skills-icon'></i>
+
+            <div>
+              <h3 className="skills__name">Git</h3>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Frontend
