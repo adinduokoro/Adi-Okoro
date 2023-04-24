0cc3b53d19164892fdf675df22eae0bbca349dd5
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2023-04-24 14:02:43 -0400

    Header
    Modifications and Updates

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,68 +1,46 @@
 import React, { useState } from 'react'
 import "./header.css"
 import { Icon } from '@iconify/react';
+import { navs } from './data';
 
 const Header = () => {
   const [toggle, setToggle] = useState(false) 
+
+  // Handles menu you close
+  const closeMenu = () => {
+    setToggle(false)
+  }
   
 
   return (
     <header className="header">
       <nav className="nav container">
+
+        {/* Site logo */}
         <a href="index.html" className="nav__logo">ADI</a>
 
+        {/* Nav menu & mobile menu */}
         <div className={toggle ? "nav__menu show-menu" : "nav__menu"}>
           <ul className='nav__list grid'>
-
-            <li className="nav__item">
-              <a href="#home" className="nav__link active-link">
-                <Icon icon="uil:estate" className='nav__icon'/>
-                Home
-              </a>
-            </li>
-
-            <li className="nav__item">
-              <a href="#about" className="nav__link">
-                <Icon icon="mdi:user-outline" className='nav__icon'/>
-                About
-              </a>
-            </li>
-
-            <li className="nav__item">
-              <a href="#skills" className="nav__link">
-                <Icon icon="uil:file-alt" className='nav__icon'/>
-                Skills
-              </a>
-            </li>
-
-            <li className="nav__item">
-              <a href="#services" className="nav__link">
-                <Icon icon="mdi:briefcase-outline" className='nav__icon'/>
-                Services
-              </a>
-            </li>
-
-            <li className="nav__item">
-              <a href="#portfolio" className="nav__link">
-                <Icon icon="uil:scenery" className='nav__icon'/>
-                Portfolio
-              </a>
-            </li>
-
-            <li className="nav__item">
-              <a href="#contact" className="nav__link">
-              <Icon icon="uil:message" className='nav__icon'/>
-                Contact
-              </a>
-            </li>
+            {navs.map((nav, index) => {
+              return(
+                <li className="nav__item" key={index}>
+                  <a href={nav.path} className="nav__link active-link" onClick={closeMenu}>
+                    <Icon icon={nav.icon} className='nav__icon'/>
+                    {nav.text}
+                  </a>
+                </li>
+              )
+            })}
           </ul>
 
+          {/* Nav controls */}
           <Icon icon="uil:times" className='nav__close' onClick={() => setToggle(!toggle)}/>
         </div>
-
         <div className="nav__toggle" onClick={() => setToggle(!toggle)}>
           <Icon icon="uil:apps" />
         </div>
+
       </nav>
     </header>
   )
---
+++ src/components/header/data.jsx
@@ -0,0 +1,34 @@
+export const navs = [
+  {
+    text: "Home",
+    path: "#home",
+    icon: "uil:estate"
+  },
+  {
+    text: "About",
+    path: "#about",
+    icon: "uil:estate"
+  },
+  {
+    text: "Skills",
+    path: "#skills",
+    icon: "uil:file-alt"
+  },
+  {
+    text: "Services",
+    path: "#services",
+    icon: "mdi:briefcase-outline"
+  },
+  {
+    text: "Portfolio",
+    path: "#portfolio",
+    icon: "uil:scenery"
+  },
+  {
+    text: "Contact",
+    path: "#contact",
+    icon: "uil:message"
+  },
+]
+
+export default { navs }
