a1a127eff04f41af485d868ff6ecd114f19fb685
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2023-04-24 15:44:11 -0400

    About
    Modifying the about section.

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -1,26 +1,18 @@
 import React from 'react'
+import { cardBox } from './data'
 
 const Info = () => {
   return (
     <div className="about__info grid">
-      <div className="about__box">
-        <i class='bx bx-star about__icon' ></i>
-        <h3 className="about__title">Experience</h3>
-        <span className="about__subtitle">10+ Years Working</span>
-      </div>
-
-      <div className="about__box">
-        <i class='bx bx-star about__icon'></i>
-        <h3 className="about__title">Experience</h3>
-        <span className="about__subtitle">10+ Years Working</span>
-      </div>
-
-      <div className="about__box">
-        <i class='bx bx-star about__icon'></i>
-        <h3 className="about__title">Experience</h3>
-        <span className="about__subtitle">10+ Years Working</span>
-      </div>
-
+      {cardBox.map((box, index) => {
+        return(
+          <div className="about__box" key={index}>
+            <i className={box.icon + " about__icon"} ></i>
+            <h3 className="about__title">{box.title}</h3>
+            <span className="about__subtitle">{box.subtitle}</span>
+          </div>
+        )
+      })}
     </div>
   )
 }
---
+++ src/components/about/data.jsx
@@ -0,0 +1,19 @@
+export const cardBox =  [
+  {
+    icon: "bx bx-star",
+    title: "Experience",
+    subtitle: "8+ Years",
+  },
+  {
+    icon: "bx bx-star",
+    title: "Experience",
+    subtitle: "10+ Years Working",
+  },
+  {
+    icon: "bx bx-star",
+    title: "Experience",
+    subtitle: "10+ Years Working",
+  },
+]
+
+export default { cardBox }
